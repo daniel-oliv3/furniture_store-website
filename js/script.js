54cbb5af5fb77717e5fd97b2bb38448cc4b83b80
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-07 12:32:10 -0300

    Update slide Javascript prev next button

--- js/script.js
+++ js/script.js
@@ -38,3 +38,18 @@ document.querySelector('#search-btn').onclick = () => {
 window.onscroll = () => {
     searchForm.classList.remove('active');
 }
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next (){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev (){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
